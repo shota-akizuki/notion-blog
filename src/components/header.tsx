66f2bdd9638e11fa639b53b685b625287caded2f
Shota Akizuki <64979430+shota-akizuki@users.noreply.github.com>
2020-12-26 01:08:07 +0900

    ブログタイトルの修正など

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -22,14 +22,14 @@ export default ({ titlePre = '' }) => {
   return (
     <header className={styles.header}>
       <Head>
-        <title>{titlePre ? `${titlePre} |` : ''} My Notion Blog</title>
+        <title>{titlePre ? `${titlePre} |` : ''} Shota Developer Blog</title>
         <meta
           name="description"
           content="An example Next.js site using Notion for the blog"
         />
-        <meta name="og:title" content="My Notion Blog" />
+        <meta name="og:title" content="Shota Developer Blog" />
         <meta property="og:image" content={ogImageUrl} />
-        <meta name="twitter:site" content="@_ijjk" />
+        <meta name="twitter:site" content="@shota_akizuki" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image" content={ogImageUrl} />
       </Head>
