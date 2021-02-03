746364bd57498119ead3b1d7e73e661fcc1a74dc
Shota Akizuki <64979430+shota-akizuki@users.noreply.github.com>
2021-02-03 14:02:15 +0900

    add: ProductsからApp Storeへのリンクを追加

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -10,7 +10,12 @@ export default () => (
       <div className={contactStyles.avatar}>
         <img src="/widget_app.png" alt="pointed_star" height="30" width="120" />
       </div>
-      <a className={contactStyles.links}>スマホでFlutter(iOS)</a>
+      <a
+        href="https://apps.apple.com/jp/app/%E3%82%B9%E3%83%9E%E3%83%9B%E3%81%A7flutter/id1551776146"
+        className={contactStyles.links}
+      >
+        スマホでFlutter(iOS)
+      </a>
       <p className={contactStyles.links}>
         （KBOYのFlutter大学共同開発チームで開発）
       </p>
