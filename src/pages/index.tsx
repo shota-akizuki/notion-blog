573fb53fc830a0a35f9c1aff271d75b66e3df41c
Shota Akizuki <64979430+shota-akizuki@users.noreply.github.com>
2021-02-07 03:39:06 +0900

    update: 文字のスタイルを調整

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,11 @@ export default () => (
   <>
     <Header titlePre="Home" />
     <div className={sharedStyles.layout}>
-      <h1>Shota Akizuki</h1>
+      <h1 style={{ fontSize: 36 }}>Shota Akizuki</h1>
       <div className={contactStyles.avatar}>
         <img src="/pointed-star.png" alt="pointed_star" width="120" />
       </div>
-      <h1>Develeper Web Site</h1>
+      <h1 style={{ fontSize: 36 }}>Develeper Web Site</h1>
     </div>
   </>
 )
--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,9 +1,5 @@
 import Header from '../components/header'
-import ExtLink from '../components/ext-link'
 import sharedStyles from '../styles/shared.module.css'
-import contactStyles from '../styles/contact.module.css'
-import GitHub from '../components/svgs/github'
-import Envelope from '../components/svgs/envelope'
 
 export default () => (
   <>
