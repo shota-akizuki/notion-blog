c1e3a56c9313891a9b364a4fbe75a9a5cc4ed9d3
Shota Akizuki <64979430+shota-akizuki@users.noreply.github.com>
2021-02-07 03:20:48 +0900

    update: スキルの文言を修正

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -5,22 +5,10 @@ import contactStyles from '../styles/contact.module.css'
 import GitHub from '../components/svgs/github'
 import Envelope from '../components/svgs/envelope'
 
-const contacts = [
-  {
-    Comp: GitHub,
-    alt: 'github icon',
-    link: 'https://github.com/shota-akizuki',
-  },
-  {
-    Comp: Envelope,
-    alt: 'envelope icon',
-    link: 'mailto:shota0609akizuki@gmail.com',
-  },
-]
-
 export default () => (
   <>
     <Header titlePre="Skills" />
+
     <div className={sharedStyles.layout}>
       <div>
         <img
@@ -30,7 +18,9 @@ export default () => (
           width="400"
         />
       </div>
-      <h1 style={{ marginTop: 0, fontSize: 30 }}>Flutter App Development</h1>
+      <h1 style={{ marginTop: 0, fontSize: 24 }}>
+        Flutter&Dart App Development
+      </h1>
     </div>
   </>
 )
