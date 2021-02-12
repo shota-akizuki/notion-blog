45ae0e25b789edf69cba6f3a5cc63a5fea8a40ce
Shota Akizuki <64979430+shota-akizuki@users.noreply.github.com>
2021-02-12 21:56:56 +0900

    add: Material-UIでDividerを追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,20 @@
 import Header from '../components/header'
 import sharedStyles from '../styles/shared.module.css'
+import Divider from '@material-ui/core/Divider'
+import contactStyles from '../styles/contact.module.css'
 
 export default () => (
   <>
     <Header titlePre="Home" />
     <div className={sharedStyles.layout}>
       <h1 style={{ fontSize: 40 }}>Shota Akizuki</h1>
-      <h2 style={{ marginTop: 24, fontSize: 24 }}>- Developer Website -</h2>
+      <h2 style={{ margin: 40 }}>- Developer Website -</h2>
       <div className={sharedStyles.layout}>
-        <h2 style={{ marginTop: 60, fontSize: 24 }}>
+        <Divider style={{ margin: 56 }} />
+        <h2
+          style={{ marginTop: 60, fontSize: 24 }}
+          className={contactStyles.name}
+        >
           Flutter Mobile App Development
         </h2>
         <div>
