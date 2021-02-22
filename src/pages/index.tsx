5261dd1b800ada3b8324264e48b54639c2e2c8df
Shota Akizuki <64979430+shota-akizuki@users.noreply.github.com>
2021-02-23 00:23:06 +0900

    delete: flutter-logoを削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import sharedStyles from '../styles/shared.module.css'
 import Divider from '@material-ui/core/Divider'
 import NextLogo from '../components/svgs/nextjs-3'
 import ReactLogo from '../components/svgs/react-2'
-import FlutterLogo from '../components/svgs/flutter-logo'
 
 export default () => {
   return (
@@ -28,18 +27,6 @@ export default () => {
             marginBottom: -16,
             fill: 'var(--color-text)',
           }}
-          className={sharedStyles.logo}
-        />
-        <h2
-          style={{
-            marginTop: 64,
-            fontSize: 24,
-          }}
-        >
-          Flutter Mobile App Development
-        </h2>
-        <FlutterLogo
-          style={{ marginTop: 56, marginBottom: -24 }}
           className={sharedStyles.logo}
         />
       </div>
