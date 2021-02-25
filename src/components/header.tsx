3bb76b72a120217963eaea739b99db11f329a694
Shota Akizuki <64979430+shota-akizuki@users.noreply.github.com>
2021-02-26 04:16:32 +0900

    Fix navItems

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,7 +8,7 @@ const navItems: { label: string; page?: string; link?: string }[] = [
   { label: 'Home', page: '/' },
   { label: 'Products', page: '/products' },
   { label: 'Contact', page: '/contact' },
-  { label: 'Privacy Policy', page: '/blog' },
+  { label: 'Privacy', page: '/blog' },
 ]
 
 const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'
